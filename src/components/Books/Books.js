08141ab4f52533ef003bb04d6c904a1f55cea64f
Ferew Tucho <ferewtucho@gmail.com>
2018-12-31 12:51:36 -0500

    created book and search area components

---
+++ src/components/Books/Books.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import SearchArea from "../Search/SearchArea";
+
+class Books extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <SearchArea />
+      </div>
+    );
+  }
+}
+
+export default Books;
---
+++ src/components/Search/SearchArea.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchArea() {
+  return (
+    <div className="search-area">
+      <form action="">
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default SearchArea;
