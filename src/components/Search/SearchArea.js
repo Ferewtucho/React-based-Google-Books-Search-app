c9404050cf95fcd191043490e13f7318610ea092
Ferew Tucho <ferewtucho@gmail.com>
2018-12-31 12:56:20 -0500

    added some css on search and book components

--- src/components/Search/SearchArea.js
+++ src/components/Search/SearchArea.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./SearchArea.css";
 
 function SearchArea() {
   return (
     <div className="search-area">
       <form action="">
         <input type="text" />
+        <button type="submit">Search</button>
       </form>
     </div>
   );
